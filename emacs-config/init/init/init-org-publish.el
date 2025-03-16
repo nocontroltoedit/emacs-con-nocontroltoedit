5eb10801aa440535bc60a6dbce944f0e43b0c464
nocontroltoedit <nocontroltoedit@gmail.com>
2025-03-16 19:35:09 +0000

    changed footer using html postamble format string

--- emacs-config/init/init/init-org-publish.el
+++ emacs-config/init/init/init-org-publish.el
@@ -1,9 +1,87 @@
 (use-package ox-publish)
 
-(setq nocontroltoedit-github-footer
-      "updated on %d")
+
+;; (:export-options :back-end :translate-alist :exported-data
+;; 		 :input-buffer :input-file :html-doctype
+;; 		 :html-container :html-content-class :description
+;; 		 :keywords :html-html5-fancy :html-link-use-abs-url
+;; 		 :html-link-home :html-link-up :html-mathjax
+;; 		 :html-equation-reference-format :html-postamble
+;; 		 :html-preamble :html-head :html-head-extra :subtitle
+;; 		 :html-head-include-default-style
+;; 		 :html-head-include-scripts
+;; 		 :html-allow-name-attribute-in-anchors :html-divs
+;; 		 :html-checkbox-type :html-extension
+;; 		 :html-footnote-format :html-footnote-separator
+;; 		 :html-footnotes-section :html-format-drawer-function
+;; 		 :html-format-headline-function
+;; 		 :html-format-inlinetask-function :html-home/up-format
+;; 		 :html-indent :html-infojs-options
+;; 		 :html-infojs-template :html-inline-image-rules
+;; 		 :html-link-org-files-as-html :html-mathjax-options
+;; 		 :html-mathjax-template
+;; 		 :html-metadata-timestamp-format
+;; 		 :html-postamble-format :html-preamble-format
+;; 		 :html-prefer-user-labels :html-self-link-headlines
+;; 		 :html-table-align-individual-fields
+;; 		 :html-table-caption-above :html-table-data-tags
+;; 		 :html-table-header-tags
+;; 		 :html-table-use-header-tags-for-first-column
+;; 		 :html-tag-class-prefix :html-text-markup-alist
+;; 		 :html-todo-kwd-class-prefix :html-toplevel-hlevel
+;; 		 :html-use-infojs :html-validation-link :html-viewport
+;; 		 :html-inline-images :html-table-attributes
+;; 		 :html-table-row-open-tag :html-table-row-close-tag
+;; 		 :html-xml-declaration :html-wrap-src-lines
+;; 		 :html-klipsify-src :html-klipse-css :html-klipse-js
+;; 		 :html-klipse-selection-script :infojs-opt :creator
+;; 		 :with-latex :latex-header :title :date :author :email
+;; 		 :language :select-tags :exclude-tags :headline-levels
+;; 		 :preserve-breaks :section-numbers :time-stamp-file
+;; 		 :with-archived-trees :with-author :expand-links
+;; 		 :with-broken-links :with-clocks :with-creator
+;; 		 :with-date :with-drawers :with-email :with-emphasize
+;; 		 :with-entities :with-fixed-width :with-footnotes
+;; 		 :with-inlinetasks :with-planning :with-priority
+;; 		 :with-properties :with-smart-quotes
+;; 		 :with-special-strings :with-special-rows
+;; 		 :with-statistics-cookies :with-sub-superscript
+;; 		 :with-toc :with-tables :with-tags :with-tasks
+;; 		 :with-timestamps :with-title :with-todo-keywords
+;; 		 :with-cite-processors :cite-export :output-file
+;; 		 :base-directory :publishing-directory
+;; 		 :publishing-function :recursive :crossrefs
+;; 		 :filter-final-output :bibliography :filter-body
+;; 		 :filter-bold :filter-babel-call :filter-center-block
+;; 		 :filter-clock :filter-code :filter-diary-sexp
+;; 		 :filter-drawer :filter-dynamic-block :filter-entity
+;; 		 :filter-example-block :filter-export-block
+;; 		 :filter-export-snippet :filter-fixed-width
+;; 		 :filter-footnote-definition
+;; 		 :filter-footnote-reference :filter-headline
+;; 		 :filter-horizontal-rule :filter-inline-babel-call
+;; 		 :filter-inline-src-block :filter-inlinetask
+;; 		 :filter-italic :filter-item :filter-keyword
+;; 		 :filter-latex-environment :filter-latex-fragment
+;; 		 :filter-line-break :filter-link :filter-node-property
+;; 		 :filter-options :filter-paragraph :filter-parse-tree
+;; 		 :filter-plain-list :filter-plain-text
+;; 		 :filter-planning :filter-property-drawer
+;; 		 :filter-quote-block :filter-radio-target
+;; 		 :filter-section :filter-special-block
+;; 		 :filter-src-block :filter-statistics-cookie
+;; 		 :filter-strike-through :filter-subscript
+;; 		 :filter-superscript :filter-table :filter-table-cell
+;; 		 :filter-table-row :filter-target :filter-timestamp
+;; 		 :filter-underline :filter-verbatim
+;; 		 :filter-verse-block :ignore-list :parse-tree
+;; 		 :headline-offset :headline-numbering :id-alist
+;; 		 :citations :internal-references)
 
 
+(defun nocontroltoedit-github-footer (info)
+  (format "<p>this file was create from : %s</p>"
+	  (plist-get info :input-file)))
 
 (setq org-publish-project-alist nil)
 (add-to-list 'org-publish-project-alist
@@ -11,11 +89,16 @@
 		   :base-directory "~/wd-x1/emacs-con-nocontroltoedit/docs/org"
 		   :publishing-directory "~/wd-x1/emacs-con-nocontroltoedit/docs"
        		   :publishing-function org-html-publish-to-html
-		   :html-postamble ,nocontroltoedit-github-footer
+		   :html-postamble  "<footer><p>last modified: %C</p></footer>"
 		   :recursive t
 		   :with-toc nil
 		   ))
 
+
+;;		   :html-postamble nocontroltoedit-github-footer
 ;; :with-author nil
 ;; :with-creator nil
 ;; :time-stamp-file nil
+(save-buffer)
+(org-publish-all t)
+(magit)
